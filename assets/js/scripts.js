89cea70cb98bfe9c4bff84ec49b68359dddd26f2
atchutchi <>
2023-05-16 12:29:53 +0000

    Add variables to store and track the game, and create array to store and possible wins

---
+++ assets/js/scripts.js
@@ -0,0 +1,23 @@
+// Variables to store player scores and current round
+let playerXScore = 0;
+let playerOScore = 0;
+let currentRound = 1;
+
+// Variables to keep track of the game mode and whether play is active
+let gameMode = null;
+let gameInProgress = false;
+
+// Create an array to store each cell's value.
+let board = ["", "", "", "", "", "", "", "", ""];
+
+// Create an array with all viable winning combinations.
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
