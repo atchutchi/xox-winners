4849fbdb09931a2ca03241906bb63b6d817c87b6
atchutchi <>
2023-05-16 12:42:47 +0000

    Add 2 eventListeners to select mode and restart button and function for the game mode

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -30,3 +30,18 @@ for (let i = 1; i <= 9; i++) {
     const cell = document.getElementById(`cell${i}`);
     cell.addEventListener("click", handleCellClick);
   }
+
+// Add event listeners to the game mode selection buttons.
+document.getElementById("playerVsPlayer").addEventListener("click", () => handleGameModeSelection("playerVsPlayer"));
+document.getElementById("playerVsMachine").addEventListener("click", () => handleGameModeSelection("playerVsMachine"));
+
+// Add an event listener to the Restart button to refresh the page.
+document.getElementById("restartBtn").addEventListener("click", () => location.reload());
+
+// Function to deal with game mode selection and game launch
+function handleGameModeSelection(mode) {
+  gameMode = mode;
+  gameInProgress = true;
+  document.querySelector(".game-container").classList.add("active");
+  document.getElementById("restartBtn").classList.add("active");
+}
