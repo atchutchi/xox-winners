2a963d4c847e2875f19457fac5ee0b1893b8bcab
atchutchi <>
2023-05-29 10:51:20 +0000

    add some formating to make the code more readable and organized

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,8 @@
 // This code is adapted from the tic-tac-toe game by janschreiber on CodePen: https://codepen.io/janschreiber/pen/xZbEvM
 // Original code credit goes to janschreiber
 
+//jshint esversion: 6
+
 // Global variables to store players' scores and the current round number
 let playerXScore = 0;
 let playerOScore = 0;
@@ -32,7 +34,7 @@ const winningCombinations = [
   [2, 4, 6]
 ];
 
-// Variable to keep track of the current player. 
+// Variable to keep track of the current player.
 // It's either "X" (the first player) or "O" (the second player / the computer).
 let currentPlayer = "X";
 
@@ -42,27 +44,30 @@ const roundNumber = document.getElementById('round-number');
 const roundResult = document.getElementById('round-result');
 const continueBtn = document.getElementById('continue-btn');
 
-// Adding click event listeners to each cell of the game board.
-// When a cell is clicked, the handleCellClick function will be called.
-for (let i = 1; i <= 9; i++) {
+// Add event listeners to game elements
+function addEventListeners() {
+  // Add click event listeners to each cell of the game board.
+  // When a cell is clicked, the handleCellClick function will be called.
+  for (let i = 1; i <= 9; i++) {
     const cell = document.getElementById(`cell${i}`);
     cell.addEventListener("click", handleCellClick);
   }
-  
-  // Adding click event listeners to the game mode selection buttons.
+
+  // Add click event listeners to the game mode selection buttons.
   // When one of these buttons is clicked, the handleGameModeSelection function will be called.
   const gameModeButtons = ["playerVsPlayer", "playerVsMachine"];
   for (const buttonId of gameModeButtons) {
     document.getElementById(buttonId).addEventListener("click", () => handleGameModeSelection(buttonId));
   }
-  
-  // Adding a click event listener to the Restart button.
+
+  // Add a click event listener to the Restart button.
   // When this button is clicked, the page will be refreshed.
   document.getElementById("restartBtn").addEventListener("click", () => location.reload());
-  
-  // Adding an event listener to the Continue button.
+
+  // Add an event listener to the Continue button.
   // When this button is clicked, the continueGame function will be called.
   continueBtn.addEventListener('click', continueGame);
+}
 
 // Function to deal with game mode selection and game launch
 function handleGameModeSelection(mode) {
@@ -98,31 +103,28 @@ function handleCellClick(event) {
 
     // End the game if a player has won 5 times
     if (playerXScore >= 5 || playerOScore >= 5) {
-        endGame();
+      endGame();
     } else {
-        startNextRound();
+      startNextRound();
     }
 
     document.querySelector(".game-container").classList.remove("active");
     document.getElementById("restartBtn").classList.remove("active");
     roundDisplay.style.display = 'block'; // Show round display
- } else if (checkTie()) {
+  } else if (checkTie()) {
     startNextRound();
     roundResult.textContent = `It's a Tie!`; // Display round result
     document.querySelector(".game-container").classList.remove("active");
     document.getElementById("restartBtn").classList.remove("active");
     roundDisplay.style.display = 'block'; // Show round display
- } else {
+  } else {
     // If no one won as of yet, the current player is changed.
     currentPlayer = currentPlayer === "X" ? "O" : "X";
     if (gameMode === "playerVsMachine" && currentPlayer === "O") {
-        setTimeout(computerMove, 500); // Wait for 1 seconds before the computer makes its move
-        if (currentPlayer === "O") {
-            roundDisplay.style.display = 'block'; // Show round display
-          }
-        for (let i = 1; i <= 9; i++) {
-            document.getElementById(`cell${i}`).removeEventListener("click", handleCellClick);
-        }
+      setTimeout(computerMove, 500); // Wait for 0.5 seconds before the computer makes its move
+      for (let i = 1; i <= 9; i++) {
+        document.getElementById(`cell${i}`).removeEventListener("click", handleCellClick);
+      }
     }
   }
 }
@@ -132,20 +134,20 @@ function handleCellClick(event) {
 function computerMove() {
   // Create an array to store the indices of the empty cells on the board.
   let emptyCells = [];
-  // Loop through the board. If a cell is empty (its value is an empty string), 
+  // Loop through the board. If a cell is empty (its value is an empty string),
   // push its index to the emptyCells array.
-  for (var i = 0; i < board.length; i++) {
-    if (board[i] == '') {
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === '') {
       emptyCells.push(i);
     }
   }
-  
+
   // Initially, set the computer's move to an invalid index (-1).
   let move = -1;
-    //The computer tries to find a winning move. It looks for two similar values in a winning combination and an empty space to complete the combo.
-    //If such a move is found, it sets the move variable to the index of that empty space.
-  for(let i = 0; i < winningCombinations.length; i++) {
-    let combination = winningCombinations[i];
+  // The computer tries to find a winning move. It looks for two similar values in a winning combination and an empty space to complete the combo.
+  // If such a move is found, it sets the move variable to the index of that empty space.
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const combination = winningCombinations[i];
     if (board[combination[0]] === board[combination[1]] && board[combination[0]] !== "" && board[combination[2]] === "") {
       move = combination[2];
     } else if (board[combination[1]] === board[combination[2]] && board[combination[1]] !== "" && board[combination[0]] === "") {
@@ -159,21 +161,21 @@ function computerMove() {
   if (move === -1) {
     move = emptyCells[Math.floor(Math.random() * emptyCells.length)];
   }
-  
-  // The computer makes its move by placing its marker ("O") at the chosen index 
+
+  // The computer makes its move by placing its marker ("O") at the chosen index
   // on the board and reflecting this move on the display.
   board[move] = currentPlayer;
   document.getElementById(`cell${move + 1}`).textContent = currentPlayer;
-  
-  //After the computer makes its move, it checks if it has won the game.
-  //If it has, it increases its score, updates the scores on the display, and checks if the game is over (i.e., if it was the last round).
-  //If the game is not over, it starts the next round.
-  //If the game has resulted in a tie, it alerts the user and starts the next round.
-  //If the game is not over and it's not a tie, it simply changes the current player.
+
+  // After the computer makes its move, it checks if it has won the game.
+  // If it has, it increases its score, updates the scores on the display, and checks if the game is over (i.e., if it was the last round).
+  // If the game is not over, it starts the next round.
+  // If the game has resulted in a tie, it alerts the user and starts the next round.
+  // If the game is not over and it's not a tie, it simply changes the current player.
   if (checkWin(currentPlayer)) {
     // Increases the winner's point total
     currentPlayer === "X" ? playerXScore++ : playerOScore++;
-  
+
     // Updates the page's punctuation
     updateScores();
 
@@ -185,16 +187,16 @@ function computerMove() {
 
     // End the game if a player has won 5 times
     if (playerXScore >= 5 || playerOScore >= 5) {
-        endGame();
+      endGame();
     } else {
-        continueGame();  // Starts the continue game screen
+      continueGame(); // Starts the continue game screen
     }
 
-   // If the game has resulted in a tie, it alerts the user and starts the next round. 
- } else if (checkTie()) {
+    // If the game has resulted in a tie, it alerts the user and starts the next round.
+  } else if (checkTie()) {
     roundResult.textContent = `It's a Tie!`; // Display round result
     roundDisplay.style.display = 'block'; // Show round display
-    continueGame();  // Starts the continue game screen
+    continueGame(); // Starts the continue game screen
 
     // If the game is not over and it's not a tie, it simply changes the current player.
   } else {
@@ -202,21 +204,14 @@ function computerMove() {
     currentPlayer = currentPlayer === "X" ? "O" : "X";
   }
 
-  if (currentPlayer === "O") {
-    roundDisplay.style.display = 'block'; // Show round display
-  }
-
-  for (let i = 1; i <= 9; i++) {
-    document.getElementById(`cell${i}`).addEventListener("click", handleCellClick);
-  }
+  addEventListeners();
 }
-
 
 // Function to determine whether a player won
 function checkWin(player) {
-    return winningCombinations.some(combination =>
+  return winningCombinations.some(combination =>
     combination.every(index => board[index] === player)
-    );
+  );
 }
 
 // Function to check for a tie
@@ -226,84 +221,87 @@ function checkTie() {
 
 // Function to prepare the game for the upcoming round
 function startNextRound() {
-    // Clear the board
-    board = ["", "", "", "", "", "", "", "", ""];
-    for (let i = 1; i <= 9; i++) {
-      document.getElementById(`cell${i}`).textContent = "";
-    }
-  
-    // Player X always starts
-    currentPlayer = "X";
-    
-    // If machine should start the round
-    if (gameMode === "playerVsMachine" && currentPlayer === "O") {
-      computerMove();
-    }
+  // Clear the board
+  board = ["", "", "", "", "", "", "", "", ""];
+  for (let i = 1; i <= 9; i++) {
+    document.getElementById(`cell${i}`).textContent = "";
+  }
+
+  // Player X always starts
+  currentPlayer = "X";
+
+  // If machine should start the round
+  if (gameMode === "playerVsMachine" && currentPlayer === "O") {
+    computerMove();
+  }
 }
-      
+
 // Function to stop the game after the last round
 function endGame() {
-    gameInProgress = false;
-  
-    if (playerXScore >= 5) {
-      alert("Player X wins the game!");
-      roundResult.textContent = "Player X Won!";
-      playerXScore = 0;
-      playerOScore = 0;
-    } else if (playerOScore >= 5) {
-      alert("Player O wins the game!");
-      roundResult.textContent = "Player O Won!";
-      playerXScore = 0;
-      playerOScore = 0;
-    } else {
-      return;
-    }
+  gameInProgress = false;
 
-    // Reset scores and current round to their initial state
+  if (playerXScore >= 5) {
+    alert("Player X wins the game!");
+    roundResult.textContent = "Player X Won!";
     playerXScore = 0;
     playerOScore = 0;
-    currentRound = 1;
+  } else if (playerOScore >= 5) {
+    alert("Player O wins the game!");
+    roundResult.textContent = "Player O Won!";
+    playerXScore = 0;
+    playerOScore = 0;
+  } else {
+    return;
+  }
 
-    // Update the scores on the UI
-    updateScores();
+  // Reset scores and current round to their initial state
+  playerXScore = 0;
+  playerOScore = 0;
+  currentRound = 1;
 
-    // Hide the game board, the round display and the restart button after a delay of 1 second
-    setTimeout(() => {
-      document.querySelector(".game-container").classList.remove("active");
-      document.getElementById("restartBtn").classList.remove("active");
-      roundDisplay.style.display = 'none'; 
+  // Update the scores on the UI
+  updateScores();
 
-      // Show the game setup section for game mode selection
-      document.getElementById("game-setup").style.display = 'block'; 
+  // Hide the game board, the round display and the restart button after a delay of 1 second
+  setTimeout(() => {
+    document.querySelector(".game-container").classList.remove("active");
+    document.getElementById("restartBtn").classList.remove("active");
+    roundDisplay.style.display = 'none';
 
-      // Prepare the board for the next round
-      startNextRound();
-    }, 1000);  // Wait for 1 second before transitioning to the game setup state
+    // Show the game setup section for game mode selection
+    document.getElementById("game-setup").style.display = 'block';
+
+    // Prepare the board for the next round
+    startNextRound();
+  }, 1000);  // Wait for 1 second before transitioning to the game setup state
 }
 
 // Function to update the scores
 function updateScores() {
-    document.getElementById("playerXScore").textContent = playerXScore;
-    document.getElementById("playerOScore").textContent = playerOScore;
+  document.getElementById("playerXScore").textContent = playerXScore;
+  document.getElementById("playerOScore").textContent = playerOScore;
 }
-  
-  // Function to start a new round
-  function startNewRound() {
-    // Increment current round and update the display
-    currentRound++;
-    roundNumber.textContent = `Round ${currentRound}`;
-  
-    // Prepare the game for the upcoming round
-    startNextRound();
-}
-  
-  // Add continueGame function
-  function continueGame() {
-    startNewRound();
-    roundDisplay.style.display = 'none'; // Hide round display
-    document.querySelector(".game-container").classList.add("active");
-    document.getElementById("restartBtn").classList.add("active");
-}
-  
-  // Initialize the round number
+
+// Function to start a new round
+function startNewRound() {
+  // Increment current round and update the display
+  currentRound++;
   roundNumber.textContent = `Round ${currentRound}`;
+
+  // Prepare the game for the upcoming round
+  startNextRound();
+}
+
+// Add continueGame function
+function continueGame() {
+  startNewRound();
+  roundDisplay.style.display = 'none'; // Hide round display
+  document.querySelector(".game-container").classList.add("active");
+  document.getElementById("restartBtn").classList.add("active");
+}
+
+// Initialize the round number
+roundNumber.textContent = `Round ${currentRound}`;
+
+// Add event listeners to game elements
+addEventListeners();
